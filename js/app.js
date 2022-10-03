60a74e2f03f353a001230740e8ea3c9f380a8856
franq01 <carreone705@gmail.com>
2022-10-03 11:06:40 -0500

    hola

--- js/app.js
+++ js/app.js
@@ -4,14 +4,30 @@ const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 
+let listadoCarrito = [];
 
 const agregarCurso = (e)=>{
     e.preventDefault();
+    listadoCarrito = [...listadoCarrito, curso]
+    console.log
     if(e.target.classList.contains('agregar-carrito')){
-        console.log("Presionaste el boton");
+        const curso = e.target.parentElement.parentElement;
+        const infoCurso = {
+            imagen: curso.querySelector('img').src,
+            nombre: curso.querySelector('h4').textContent,
+            precio: curso.querySelector('p.precio').textContent,
+            id: curso.querySelector('.agregar-carrito').getAttribute('data-id'),    
+            cantidad:1
+        }
+        console.log("e.target.parentElement");
+        agregarCarrito(infoCurso);
     }
 }
 
+
+const agregarCarrito = curso =>{
+    console.log(listadoCarrito);
+}
 
 const cargarEventListener = ()=>{
     /// agregar funcion de carga de cursos al carrito 
